11868855fdcf2c89f2da01b4abc3b799143afa3d
Tomas <tomas@tomasps.com>
2025-07-08 00:59:03 +0200

    Update personal information and links throughout the portfolio

--- src/Application/UI/components/LoadingScreen.tsx
+++ src/Application/UI/components/LoadingScreen.tsx
@@ -149,16 +149,16 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                         <div style={styles.logoContainer}>
                             <div>
                                 <p style={styles.green}>
-                                    <b>Heffernan,</b>{' '}
+                                    <b>Tomas,</b>{' '}
                                 </p>
                                 <p style={styles.green}>
-                                    <b>Henry Inc.</b>
+                                    <b>Palma Inc.</b>
                                 </p>
                             </div>
                         </div>
                         <div style={styles.headerInfo}>
                             <p>Released: 01/13/2000</p>
-                            <p>HHBIOS (C)2000 Heffernan Henry Inc.,</p>
+                            <p>HHBIOS (C)2000 Tomas Palma Inc.,</p>
                         </div>
                     </div>
                     <div style={styles.body} className="loading-screen-body">
@@ -195,7 +195,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                             <p>
                                 All Content Loaded, launching{' '}
                                 <b style={styles.green}>
-                                    'Henry Heffernan Portfolio Showcase'
+                                    'Tomas Palma Portfolio Showcase'
                                 </b>{' '}
                                 V1.0
                             </p>
@@ -221,13 +221,13 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                 })}
             >
                 <div style={styles.startPopup}>
-                    {/* <p style={styles.red}>
+                    <p style={styles.red}>
                         <b>THIS SITE IS CURRENTLY A W.I.P.</b>
                     </p>
                     <p>But do enjoy what I have done so far :)</p>
                     <div style={styles.spacer} />
-                    <div style={styles.spacer} /> */}
-                    <p>Henry Heffernan Portfolio Showcase 2022</p>
+                    <div style={styles.spacer} />
+                    <p>Tomas Palma Portfolio Showcase 2022</p>
                     {mobileWarning && (
                         <>
                             <br />
